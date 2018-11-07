1d4f46c93404193091403285abafdf9bfd45ed1e
PSharlan <pavel.sharlan@gmail.com>
2018-11-08 00:07:58 +0300

    feat: add multi module project

---
+++ catalog/src/test/java/com/netcracker/sharlan/CatalogApplication.java
@@ -0,0 +1,7 @@
+package com.netcracker.sharlan;
+
+public class CatalogApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ customer/src/main/java/com/netcracker/sharlan/CustomerApplication.java
@@ -0,0 +1,7 @@
+package com.netcracker.sharlan;
+
+public class CustomerApplication {
+    public static void main(String[] args) {
+
+    }
+}
