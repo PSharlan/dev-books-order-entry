b3182d0b8521b027b5d46431c65d57bbfb551281
PSharlan <pavel.sharlan@gmail.com>
2019-01-10 21:24:52 +0300

    catalog module
    fix: import @Transactional

--- catalog/src/main/java/com/netcracker/sharlan/service/CategoryServiceImpl.java
+++ catalog/src/main/java/com/netcracker/sharlan/service/CategoryServiceImpl.java
@@ -4,8 +4,8 @@ import com.netcracker.sharlan.dao.CategoryDao;
 import com.netcracker.sharlan.entities.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.Set;
 
 @Service
